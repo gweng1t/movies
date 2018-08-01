5806b01d26c4e657835e369565dec5972cd980d4
Gwekkeo <gwendoline.cassand@gmail.com>
2018-08-01 16:02:29 +0200

    ajout2 - avant push heroku1

--- db/migrate/20180801105602_create_movies.rb
+++ db/migrate/20180801105602_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :title
-      t.integer :release_year
-      t.string :director
+    	t.string :title
+		t.integer :release_year
+		t.string :director
 
-      t.timestamps
+		t.timestamps
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Movie.create(title: "Star Wars", release_year: 1994, director: "Mr.Alphone")
-Movie.create(title: "Logan", release_year: 2005, director: "Dany")
-Movie.create(title: "Pirates des Caraibes", release_year: 2006, director: "Jack Sparrow")
-Movie.create(title: "Sorcier Waverly place", release_year: 2008, director: "Selena")
-Movie.create(title: "Hulk", release_year: 2002, director: "Ms.hulky")
+Movie.create(title: "Star Wars", release_year: 1994, director_id: Director.create(first_name: "George", last_name: "Lucas").id)
+Movie.create(title: "Logan", release_year: 2005, director_id: Director.create(first_name: "James", last_name: "Mangold").id)
+Movie.create(title: "Pirates des Caraibes", release_year: 2006, director_id: Director.create(first_name: "Jack", last_name: "Sparrow").id)
+Movie.create(title: "Sorcier Waverly place", release_year: 2008, director_id: Director.create(first_name: "Todd", last_name: "J.Greenwald").id)
+Movie.create(title: "Hulk", release_year: 2002, director_id: Director.create(first_name: "Ang", last_name: "Lee").id)
