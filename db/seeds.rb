b50dab8fb7b9266f41fdf6810d6798b61311ba92
Gwekkeo <gwendoline.cassand@gmail.com>
2018-08-01 14:34:43 +0200

    ajout1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Movie.create(title: "Star Wars", release_year: 1994, director: "Mr.Alphone")
+Movie.create(title: "Logan", release_year: 2005, director: "Dany")
+Movie.create(title: "Pirates des Caraibes", release_year: 2006, director: "Jack Sparrow")
+Movie.create(title: "Sorcier Waverly place", release_year: 2008, director: "Selena")
+Movie.create(title: "Hulk", release_year: 2002, director: "Ms.hulky")
